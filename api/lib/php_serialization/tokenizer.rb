5b15adc439763e8116f69f4f394d2ab508a52800
SendPulse Tech Team <tech@sendpulse.com>
2015-05-29 09:54:07 +0300

    Added library for Ruby serialize like PHP (https://github.com/divoxx/ruby-php-serialization)

---
+++ api/lib/php_serialization/tokenizer.rb
@@ -0,0 +1,21 @@
+module PhpSerialization
+  class Tokenizer
+    def initialize(string)
+      @string = string
+    end
+
+    def each
+      while !@string.empty?
+        token = case @string
+        when /\A-?[0-9]+(\.[0-9]+)?/m then yield([:NUMBER, $&])
+        when /\A"((?:(?:"(?![;:]))*|(?:[^"]))*)"(?=[:;])/m then yield([:STRING, $1])
+        when /\A[^\s]/m             then yield([$&, $&])
+        end
+
+        @string = $'
+      end
+
+      yield([false, '$'])
+    end
+  end
+end
